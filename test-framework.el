199de538158fd467d6c0e36851655cb272d6328b
Vegard Øye <vegard_oye@hotmail.com>
2017-10-23 16:51:27 +0200

    Add license (closes #1)

--- test-framework.el
+++ test-framework.el
@@ -276,6 +276,21 @@
 ;; ensure that it is never called outside a test, otherwise it will
 ;; not be released (unless wrapped in `with-mocks-and-stubs').
 
+;;; License:
+
+;; This file is free software; you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation; either version 3, or (at your option)
+;; any later version.
+
+;; This file is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 ;;; Code:
 
 (eval-when-compile
